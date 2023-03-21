da5a9b70d4cb7982edfe2af64744e0cd45806309
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-03-21 17:33:31 +0300

    add db model for complaints;

--- property/admin.py
+++ property/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Flat
+from .models import Flat, Report
 
 
 @admin.register(Flat)
@@ -9,3 +9,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building',)
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', 'flat',)
+    list_display = ('author', 'flat', 'text')
--- property/models.py
+++ property/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 
@@ -50,3 +51,14 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Report(models.Model):
+    author = models.ForeignKey(User, verbose_name='Автор жалобы', on_delete=models.CASCADE)
+    flat = models.ForeignKey(Flat, verbose_name='Объект', on_delete=models.CASCADE)
+    text = models.TextField('Текст жалобы')
+
+    def __str__(self):
+        return f'{self.author}, {self.flat} ({self.text})'
+
+
